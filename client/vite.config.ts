9935557247bc0837a87ad0965acbde8bab67c569
gabipayan <gapayan26@gmail.com>
2025-05-27 19:06:48 -0600

    Update .gitignore, server environment variables, client dependencies, and Vite configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,12 +10,8 @@ export default defineConfig({
         target: 'http://localhost:3001',
         changeOrigin: true,
         secure: false,
-      },
-      '/auth': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false
-      },
+      }
     },
   },
+  base: '/',
 });
