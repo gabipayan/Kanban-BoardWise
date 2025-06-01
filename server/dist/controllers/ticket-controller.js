d3b579dba1a6aabe5e1de612074e7f24460d384c
gabipayan <gapayan26@gmail.com>
2025-05-31 22:44:32 -0600

    feat: Add CRUD operations for tickets and users in controllers

---
+++ server/dist/controllers/ticket-controller.js
@@ -0,0 +1,96 @@
+import { Ticket } from '../models/ticket.js';
+import { User } from '../models/user.js';
+// GET /tickets
+export const getAllTickets = async (_req, res) => {
+    console.log("HIT all tickets endpoint");
+    try {
+        const tickets = await Ticket.findAll({
+            include: [
+                {
+                    model: User,
+                    as: 'assignedUser', // This should match the alias defined in the association
+                    attributes: ['username'], // Including only the username attribute
+                },
+            ],
+        });
+        return res.json(tickets);
+    }
+    catch (error) {
+        console.error('Error fetching tickets:', error);
+        return res.status(500).json({ message: error.message });
+    }
+};
+// GET /tickets/:id
+export const getTicketById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const ticket = await Ticket.findByPk(id, {
+            include: [
+                {
+                    model: User,
+                    as: 'assignedUser', // This should match the alias defined in the association
+                    attributes: ['username'], // Including only the username attribute
+                },
+            ],
+        });
+        if (ticket) {
+            res.json(ticket);
+        }
+        else {
+            res.status(404).json({ message: 'Ticket not found' });
+        }
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+// POST /tickets
+export const createTicket = async (req, res) => {
+    const { name, status, description, assignedUserId } = req.body;
+    try {
+        const newTicket = await Ticket.create({ name, status, description, assignedUserId });
+        res.status(201).json(newTicket);
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+// PUT /tickets/:id
+export const updateTicket = async (req, res) => {
+    const { id } = req.params;
+    const { name, status, description, assignedUserId } = req.body;
+    try {
+        const ticket = await Ticket.findByPk(id);
+        if (ticket) {
+            ticket.name = name;
+            ticket.status = status;
+            ticket.description = description;
+            ticket.assignedUserId = assignedUserId;
+            await ticket.save();
+            res.json(ticket);
+        }
+        else {
+            res.status(404).json({ message: 'Ticket not found' });
+        }
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+// DELETE /tickets/:id
+export const deleteTicket = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const ticket = await Ticket.findByPk(id);
+        if (ticket) {
+            await ticket.destroy();
+            res.json({ message: 'Ticket deleted' });
+        }
+        else {
+            res.status(404).json({ message: 'Ticket not found' });
+        }
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
